7f54b59a6028a76cf6317410f32157e9dee83a6e
kawasima <kawasima1016@gmail.com>
2016-04-04 18:22:35 +0900

    Added a holidays cache

--- ex01-business-rules/src/test/java/kata/ex01/DiscountServiceTest.java
+++ ex01-business-rules/src/test/java/kata/ex01/DiscountServiceTest.java
@@ -31,7 +31,7 @@ public class DiscountServiceTest {
     public void test平日朝夕割引() {
         HighwayDrive drive = new HighwayDrive();
         drive.setEnteredAt(LocalDateTime.of(2016, 3, 31, 23, 0));
-        drive.setEnteredAt(LocalDateTime.of(2016, 4, 1, 6, 30));
+        drive.setExitedAt(LocalDateTime.of(2016, 4, 1, 6, 30));
         drive.setDriver(driver(10));
         drive.setVehicleFamily(STANDARD);
         drive.setRouteType(RURAL);
@@ -42,8 +42,8 @@ public class DiscountServiceTest {
     @Test
     public void test休日朝夕は休日割が適用される() {
         HighwayDrive drive = new HighwayDrive();
-        drive.setEnteredAt(LocalDateTime.of(2016, 3, 31, 23, 0));
-        drive.setEnteredAt(LocalDateTime.of(2016, 4, 2, 6, 30));
+        drive.setEnteredAt(LocalDateTime.of(2016, 4, 1, 23, 0));
+        drive.setExitedAt(LocalDateTime.of(2016, 4, 2, 6, 30));
         drive.setDriver(driver(10));
         drive.setVehicleFamily(STANDARD);
         drive.setRouteType(RURAL);
