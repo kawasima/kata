cf744c112f459be8629405fcab9f8f83e82bb24d
kawasima <kawasima1016@gmail.com>
2019-05-21 19:35:27 +0900

    Write documentation on the Frame interface

--- ex03-bowling/src/main/java/kata/ex03/Frame.java
+++ ex03-bowling/src/main/java/kata/ex03/Frame.java
@@ -4,8 +4,25 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 public interface Frame {
+    /**
+     * このフレームのスコアを計算し返します.
+     *
+     * @return フレームのスコア
+     */
     int getScore();
+
+    /**
+     * このフレームに含まれる投球(roll)と以降のフレームに含まれる投球のストリームを返します.
+     *
+     * @return 投球のストリーム
+     */
     Stream<Roll> rollStream();
+
+    /**
+     * このフレームの次のフレームを返します.
+     *
+     * @return 次のフレーム(Optional)
+     */
     Optional<Frame> getNextFrame();
     void setNextFrame(Frame frame);
     default String toFormattedString() {
